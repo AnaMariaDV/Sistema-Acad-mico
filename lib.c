93c5ee881788ce28c276be6e9aa7696905c76a65
Ana Maria <aninhatexas18@gmail.com>
2025-08-13 18:07:09 -0300

    Versão 1

---
+++ lib.c
@@ -0,0 +1,17 @@
+#include "lib.h"
+
+struct pessoa
+{
+    int dia;
+    int mes;
+    int ano;
+    int idade;
+}
+int calcula_idade(struct pessoa, int ano)
+{
+
+    int idade;
+    idade=ano_atual-pessoa.ano;
+
+   return idade; 
+}
---
+++ lib.h
@@ -0,0 +1,8 @@
+#ifndef LIB_H
+#define LIB_H
+
+#include<stdio.h>
+int calcula_idade(struct pessoa);
+struct pessoa;
+
+#endif  LIB_H
